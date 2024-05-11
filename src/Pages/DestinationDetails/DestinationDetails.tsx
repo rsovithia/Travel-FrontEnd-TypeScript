7857e61ce44ca08c4f423ca48cfd359de25af93c
rsovithia <45631545+rsovithia@users.noreply.github.com>
2024-05-12 04:57:17 +0700

    api with deatil

--- src/Pages/DestinationDetails/DestinationDetails.tsx
+++ src/Pages/DestinationDetails/DestinationDetails.tsx
@@ -1,20 +1,35 @@
-import {
-  Typography,
-  Paper,
-  Box,
-  CardContent,
-  Card,
-  CardActionArea,
-  CardMedia,
-  Grid,
-  Button,
-  Container,
-} from "@mui/material";
-
-import Image from "./../../assets/image16.svg";
+import React, { useState, useEffect } from "react"; // Importing useState and useEffect
+import { Typography, Paper, Box, Container } from "@mui/material";
+import config from "../../Api/config";
 
 function DestinationDetails() {
-  // Mock data for destination details
+  const [data, setData] = useState([]); // State variable to store fetched data
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${config.apiUrl}/admin/destination/all`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${config.accessToken}`,
+          },
+        });
+
+        if (response.ok) {
+          const jsonData = await response.json();
+          setData(jsonData.data);
+        } else {
+          console.log("Failed to fetch data:", response.status);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData(); // Call the fetchData function when the component mounts
+  }, []); // Empty dependency array ensures this effect runs only once after the initial render
+
+  console.log(data);
 
   return (
     <>
@@ -71,69 +86,6 @@ function DestinationDetails() {
             />
           </Container>
         </Box>
-
-        {/* <Paper
-          sx={{
-            display: "flex",
-            justifyContent: "left",
-            padding: "0 200px   0",
-          }}
-        >
-          <Box
-            sx={{
-              marginBottom: "20px",
-              width: "250px",
-
-              height: "250px",
-              bgcolor: "red",
-            }}
-          ></Box>
-          <Box sx={{ marginBottom: "20px", padding: "40px" }}>
-            <Typography variant="h5" gutterBottom>
-              Destination Details
-            </Typography>
-            <Typography variant="body1" sx={{ marginBottom: "10px" }}>
-              <strong>Name:</strong> {destination.name}
-            </Typography>
-            <Typography variant="body1" sx={{ marginBottom: "10px" }}>
-              <strong>Location:</strong> {destination.location}
-            </Typography>
-            <Typography variant="body1" sx={{ marginBottom: "10px" }}>
-              <strong>Description:</strong> {destination.description}
-            </Typography>
-          </Box>
-        </Paper> */}
-        {/* <Paper
-          sx={{ display: "flex", justifyContent: "center", padding: "0 200px" }}
-        >
-          <Box sx={{ marginBottom: "20px" }}>
-            <Typography variant="h5" gutterBottom>
-              Learn more about AI Text Generator: What is Genius Mode? It is an
-              enhanced version of AI Chat that provides more knowledge, fewer
-              errors, improved reasoning skills, better verbal fluidity, and an
-              overall superior performance. Due to the larger AI model, Genius
-              Mode is only available via subscription to DeepAI Pro. However,
-              the added benefits often make it a worthwhile investment. What is
-              Online Mode? It is an add on that enables AI Chat to browse the
-              web for real-time information. It is a great way to learn new
-              things and explore new topics. Sign in to your DeepAI account (no
-              subscription required!) to gain access to this feature. Ideas for
-              Chatting with the AI - Can you describe the concept of relativity
-              to me in layman's terms? - What are some unique and entertaining
-              ways to celebrate a friend's anniversary? - Could you walk me
-              through how to use loops in Python? Strengths - Can recall
-              information from previous conversations to provide personalized
-              responses. - Allows users to correct any misunderstandings or
-              errors in the previous interaction. - Is programmed to refuse
-              inappropriate or harmful requests. Weaknesses - Can occasionally
-              provide incorrect information due to limitations in its training
-              data or understanding. - May inadvertently provide instructions or
-              suggestions that are harmful or biased without realizing it. -
-              Limited knowledge of current events and developments beyond the
-              training data cutoff of 2021.
-            </Typography>
-          </Box>
-        </Paper> */}
       </Box>
       <Box
         sx={{
@@ -144,38 +96,8 @@ function DestinationDetails() {
       >
         <Box sx={{ width: "60%", padding: "100px", height: "50%" }}>
           <Typography variant="h3" sx={{ textAlign: "start", color: "red" }}>
-            Your Big Title Here
+            {data.length > 0 ? `Name : ${data[0].name}` : " "}
           </Typography>
-        </Box>
-        <Box
-          sx={{
-            width: "50%",
-            padding: "50px",
-            height: "100%",
-            margin: "20px  0px 0 0",
-            bgcolor: "lightgray",
-          }}
-        >
-          <Box sx={{ marginBottom: "20px", padding: "20px" }}>
-            <Typography variant="h5" gutterBottom>
-              Learn more about AI Text Generator:
-            </Typography>
-            <Typography variant="body1">
-              What is Genius Mode? It is an enhanced version of AI Chat that
-              provides more knowledge, fewer errors, improved reasoning skills,
-              better verbal fluidity, and an overall superior performance. Due
-              to the larger AI model, Genius Mode is only available via
-              subscription to DeepAI Pro. However, the added benefits often make
-              it a worthwhile investment.
-            </Typography>
-            <Typography variant="body1">
-              What is Online Mode? It is an add-on that enables AI Chat to
-              browse the web for real-time information. It is a great way to
-              learn new things and explore new topics. Sign in to your DeepAI
-              account (no subscription required!) to gain access to this
-              feature.
-            </Typography>
-          </Box>
         </Box>
       </Box>
 
@@ -255,7 +177,6 @@ function DestinationDetails() {
               </ul>
             </Typography>
           </Box>
-          sd
         </Box>
         <Box sx={{ width: "50%", padding: "100px", height: "100%" }}>
           <Box sx={{ bgcolor: "red", width: "87vh", height: "40vh" }}></Box>
@@ -275,9 +196,8 @@ function DestinationDetails() {
             gap: "10px",
             height: "100%",
             display: "flex",
-            justifyContent: "space-between",  
-            alignItems: "center",  
-  
+            justifyContent: "space-between",
+            alignItems: "center",
           }}
         >
           {/* Each child Box with red background, fixed width, and height */}
@@ -286,100 +206,6 @@ function DestinationDetails() {
           <Box sx={{ bgcolor: "red", width: "50vh", height: "40vh" }}></Box>
         </Box>
       </Box>
-      {/* <Box
-        sx={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
-      >
-        <Grid container spacing={4} justifyContent="center">
-          <Grid item>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image="/static/images/cards/contemplative-reptile.jpg"
-                  alt="green iguana"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Lizard
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with
-                    over 6,000 species, ranging across all continents except
-                    Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-          <Grid item>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image="/static/images/cards/contemplative-reptile.jpg"
-                  alt="green iguana"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Lizard
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with
-                    over 6,000 species, ranging across all continents except
-                    Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-          <Grid item>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image="/images/contemplative-reptile.jpg"
-                  alt="Green Iguana"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Lizard
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with
-                    over 6,000 species, ranging across all continents except
-                    Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-          <Grid item>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image="/images/contemplative-reptile.jpg"
-                  alt="Green Iguana"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Lizard
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with
-                    over 6,000 species, ranging across all continents except
-                    Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        </Grid>
-      </Box> */}
     </>
   );
 }
